5112559120c65211adf6b61b8f1cf63a544ab7b3
Eiichi Sato <sato.eiichi@gmail.com>
2015-04-30 01:03:37 +0900

    add benchmark options

--- src/memcached-bench.go
+++ src/memcached-bench.go
@@ -4,15 +4,27 @@ import "os"
 import "fmt"
 import "github.com/eiiches/go-memcached/memcached"
 import "sync"
+import "flag"
+
+var (
+	concurrency int
+	requests    int
+	host        string
+	port        int
+)
 
 func main() {
-	concurrency := 4
-	requests := 1000
+	flag.IntVar(&concurrency, "concurrency", 10, "the number of simultaneous connections to use")
+	flag.IntVar(&requests, "requests", 10, "the number of requests per connection")
+	flag.StringVar(&host, "host", "localhost", "hostname")
+	flag.IntVar(&port, "port", 11211, "port")
+	flag.Parse()
+
 	var wg sync.WaitGroup
 	wg.Add(concurrency)
 	for c := 0; c < concurrency; c++ {
 		go func() {
-			client, err := memcached.NewMemcachedClient("tcp", "localhost:11212")
+			client, err := memcached.NewMemcachedClient("tcp", fmt.Sprintf("%s:%d", host, port))
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "error: %+v\n", err)
 				return
